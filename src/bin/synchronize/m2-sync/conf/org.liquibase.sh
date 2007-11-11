e663acbd9946be9dc621f606de2b1d4725793840
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-11-11 22:34:51 +0000

    [MAVENUPLOAD-1807] Add org.liquibase sync
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/tools/trunk/maven-meeper@593983 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/bin/synchronize/m2-sync/conf/org.liquibase.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CONTACTS="nathan voxland <nathan@voxland.net>"
+MODE=rsync_ssh
+
+FROM=mavensync@shell.sourceforge.net:/home/groups/l/li/liquibase/htdocs/maven-repository
+GROUP_DIR=org/liquibase/
