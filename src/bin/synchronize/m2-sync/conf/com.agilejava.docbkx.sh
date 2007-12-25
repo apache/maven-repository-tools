3057c7d74aadfd47d35c6a38f72fbe6661c1f60f
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-12-25 19:49:33 +0000

    [MAVENUPLOAD-1870] Add com.agilejava.docbkx
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/tools/trunk/maven-meeper@606804 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/bin/synchronize/m2-sync/conf/com.agilejava.docbkx.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CONTACTS="Wilfred Springer <wilfred@agilejava.com>"
+MODE=rsync_ssh
+
+FROM=mvnrepo@shell.sourceforge.net:/home/groups/d/do/docbkx-tools/htdocs/repository
+GROUP_DIR=com/agilejava/docbkx/
