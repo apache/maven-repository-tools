10785ab627ffbadcd4089a68745fca496e69fe9d
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-12-25 19:44:43 +0000

    [MAVENUPLOAD-1842] Add com.healthmarketscience
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/tools/trunk/maven-meeper@606801 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/bin/synchronize/m2-sync/conf/com.healthmarketscience.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# ibiblio repository sync script
+
+CONTACTS="openhms-bugs@lists.sourceforge.net"
+MODE=rsync_ssh
+
+FROM=maven@open.hmsonline.com:/home/repo/html/maven2
+GROUP_DIR=com/healthmarketscience/
