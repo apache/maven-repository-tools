bd792d7f8e377819df19f215b57519119a870ccc
Carlos Sanchez Gonzalez <carlos@apache.org>
2008-01-02 20:42:58 +0000

    [MAVENUPLOAD-1837] Add org.openfast
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/tools/trunk/maven-meeper@608216 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/bin/synchronize/m2-sync/conf/org.openfast.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CONTACTS="Jacob Northey <jacob_northey@users.sourceforge.net>"
+MODE=rsync_ssh
+
+FROM=mavensync@shell.sf.net:/home/groups/o/op/openfast/maven/release
+GROUP_DIR=org/openfast/
