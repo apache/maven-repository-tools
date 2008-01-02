39dc2616b4b7a8e0c565cd2083953e566b5e84a1
Carlos Sanchez Gonzalez <carlos@apache.org>
2008-01-02 20:34:46 +0000

    [MAVENUPLOAD-1866] Add net.sf.qdwizard
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/tools/trunk/maven-meeper@608211 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/bin/synchronize/m2-sync/conf/net.sf.qdwizard.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CONTACTS="Bertrand Florat <bertrand@florat.net>"
+MODE=rsync_ssh
+
+FROM=mavensync@shell.sourceforge.net:/home/groups/q/qd/qdwizard/htdocs/repository
+GROUP_DIR=net/sf/qdwizard/
