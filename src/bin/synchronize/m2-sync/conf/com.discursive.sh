2d5dccf3c368fd76e22dc77f2e1cafff2bb3166d
Carlos Sanchez Gonzalez <carlos@apache.org>
2008-01-18 03:18:52 +0000

    [MAVENUPLOAD-1894] Add com.discursive [MAVENUPLOAD-1895] Add net.sf.buildbox [MAVENUPLOAD-1896] Add org.mattressframework
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/tools/trunk/maven-meeper@613048 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/bin/synchronize/m2-sync/conf/com.discursive.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CONTACTS="Tim O'Brien <tobrien@discursive.com>"
+MODE=rsync_ssh
+
+FROM=mvnrepo@mars.discursive.com:/var/www/html/www.discursive.com/maven2
+GROUP_DIR=com/discursive/
---
+++ src/bin/synchronize/m2-sync/conf/org.mattressframework.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CONTACTS="Josh Devins <joshdevins@mattressframework.org>"
+MODE=rsync_ssh
+
+FROM=mavensync@shell.sourceforge.net:/home/groups/m/ma/mattress/htdocs/repositories/release
+GROUP_DIR=org/mattressframework/
