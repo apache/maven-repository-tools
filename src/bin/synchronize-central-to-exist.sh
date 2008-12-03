7db0dcbcd156254d912adc89e069af1a6e23e3dc
Brett Leslie Porter <brett@apache.org>
2008-12-03 07:24:38 +0000

    don't delete additional checksums, index on remote server
    git-svn-id: https://svn.apache.org/repos/asf/maven/repository-tools/trunk@722799 13f79535-47bb-0310-9956-ffa450edef68

--- src/bin/synchronize-central-to-exist.sh
+++ src/bin/synchronize-central-to-exist.sh
@@ -6,5 +6,6 @@
 
 echo ">>>>>>>>>>>>>>>>>> Syncing Maven 2.x repository to Exist"
 
-rsync -e $HOME/bin/cmd-for-rssh.sh --delete --max-delete=10 -v -z -riplt $MAVEN2_REPO/ $EXIST_USERNAME@$EXIST_SYNC_HOST:$M2_EXIST_SYNC_DIR
+rsync -e $HOME/bin/cmd-for-rssh.sh --include='.index' --include='*.asc' --include='*.md5' --include='*.sha1' -v -z -riplt $MAVEN2_REPO/ $EXIST_USERNAME@$EXIST_SYNC_HOST:$M2_EXIST_SYNC_DIR
 
+rsync -e $HOME/bin/cmd-for-rssh.sh --exclude='.index' --exclude='*.asc' --exclude='*.md5' --exclude='*.sha1' --delete --max-delete=10 -v -z -riplt $MAVEN2_REPO/ $EXIST_USERNAME@$EXIST_SYNC_HOST:$M2_EXIST_SYNC_DIR
