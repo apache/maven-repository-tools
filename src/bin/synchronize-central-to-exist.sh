ff6b981dc1b8c0eb31feaaaada7b2acb302f9608
Brett Leslie Porter <brett@apache.org>
2008-12-04 05:30:06 +0000

    better method for protecting files from deletion, and don't use max-delete
    git-svn-id: https://svn.apache.org/repos/asf/maven/repository-tools/trunk@723216 13f79535-47bb-0310-9956-ffa450edef68

--- src/bin/synchronize-central-to-exist.sh
+++ src/bin/synchronize-central-to-exist.sh
@@ -6,6 +6,4 @@
 
 echo ">>>>>>>>>>>>>>>>>> Syncing Maven 2.x repository to Exist"
 
-rsync -e $HOME/bin/cmd-for-rssh.sh --include='.index' --include='*.asc' --include='*.md5' --include='*.sha1' -v -z -riplt $MAVEN2_REPO/ $EXIST_USERNAME@$EXIST_SYNC_HOST:$M2_EXIST_SYNC_DIR
-
-rsync -e $HOME/bin/cmd-for-rssh.sh --exclude='.index' --exclude='*.asc' --exclude='*.md5' --exclude='*.sha1' --delete --max-delete=10 -v -z -riplt $MAVEN2_REPO/ $EXIST_USERNAME@$EXIST_SYNC_HOST:$M2_EXIST_SYNC_DIR
+rsync -e $HOME/bin/cmd-for-rssh.sh --filter='P .index/**' --filter='P **/*.asc' --filter='P **/*.md5' --filter='P **/*.sha1' --delete -v -z -riplt $MAVEN2_REPO/ $EXIST_USERNAME@$EXIST_SYNC_HOST:$M2_EXIST_SYNC_DIR
