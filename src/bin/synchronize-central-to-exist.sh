fe8d677ccf1c9580df004ebdba073a42f788d20d
Brett Leslie Porter <brett@apache.org>
2008-12-08 05:21:10 +0000

    don't sync .svn directories
    git-svn-id: https://svn.apache.org/repos/asf/maven/repository-tools/trunk@724261 13f79535-47bb-0310-9956-ffa450edef68

--- src/bin/synchronize-central-to-exist.sh
+++ src/bin/synchronize-central-to-exist.sh
@@ -6,4 +6,4 @@
 
 echo ">>>>>>>>>>>>>>>>>> Syncing Maven 2.x repository to Exist"
 
-rsync -e $HOME/bin/cmd-for-rssh.sh --filter='P .index/**' --filter='P **/*.asc' --filter='P **/*.md5' --filter='P **/*.sha1' --delete -v -z -riplt $MAVEN2_REPO/ $EXIST_USERNAME@$EXIST_SYNC_HOST:$M2_EXIST_SYNC_DIR
+rsync -e $HOME/bin/cmd-for-rssh.sh --filter='- **/.svn/**' --filter='P .index/**' --filter='P **/*.asc' --filter='P **/*.md5' --filter='P **/*.sha1' --delete -v -z -riplt $MAVEN2_REPO/ $EXIST_USERNAME@$EXIST_SYNC_HOST:$M2_EXIST_SYNC_DIR
