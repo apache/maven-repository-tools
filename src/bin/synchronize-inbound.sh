c4cf50676eba1eaef1fb0804d3456cea8fedf214
Brian E Fox <brianf@apache.org>
2009-01-27 02:25:34 +0000

    pull inbound sync into a separate script
    git-svn-id: https://svn.apache.org/repos/asf/maven/repository-tools/trunk@737976 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/bin/synchronize-inbound.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd $HOME/bin/synchronize/m2-sync
+svn up sync.csv
+/opt/java/sdk/current/bin/java -jar maven-meeper-1-SNAPSHOT-jar-with-dependencies.jar sync.properties sync.csv
+date > /home/maven/repository-staging/to-ibiblio/maven2/last_updated.txt
