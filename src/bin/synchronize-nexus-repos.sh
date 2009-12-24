6d92697f98dc84d41c53a158da71a040fe7c7b04
Brian E Fox <brianf@apache.org>
2009-12-24 01:38:20 +0000

    git-svn-id: https://svn.apache.org/repos/asf/maven/repository-tools/trunk@893680 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/bin/synchronize-nexus-repos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/maven/bin/synchronize/m2-sync
+svn up
+/opt/java/sdk/current/bin/java -jar maven-meeper-1-SNAPSHOT-jar-with-dependencies.jar sync.properties.nexus sync.csv.nexus
+date > /home/maven/repository-staging/to-ibiblio/maven2/last_updated.txt
