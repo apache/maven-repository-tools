3be0218d95589bf92ff0afbcaf86258bbda2a355
Brett Leslie Porter <brett@apache.org>
2010-02-16 22:24:52 +0000

    commit various changes made on the live server
    git-svn-id: https://svn.apache.org/repos/asf/maven/repository-tools/trunk@910734 13f79535-47bb-0310-9956-ffa450edef68

--- src/bin/synchronize-inbound.sh
+++ src/bin/synchronize-inbound.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/maven/bin/synchronize/m2-sync
-svn up sync.csv
+#svn up sync.csv
 /opt/java/sdk/current/bin/java -jar maven-meeper-1-SNAPSHOT-jar-with-dependencies.jar sync.properties sync.csv
 date > /home/maven/repository-staging/to-ibiblio/maven2/last_updated.txt
